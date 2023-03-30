ec219621eaa3448181c55612bcc471da7bdadb0c
Yozora09 <yozoraizumi@gmail.com>
2023-03-30 21:14:07 +0800

    DRR App

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DRR',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
